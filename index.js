0260c41285a490621e20390ba02713e990c615f6
biancajj1 <biancajj1@gmail.com>
2023-02-20 03:08:01 -0700

    about halfway done with responsiveness. Finished index,sign-in, and about page

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-Menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu(){
+
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+//Inside the media Query
+
+.hamburger.active .bar:nth-child(2){
+
+  opacity: 0;
+}
+
+.hamburger.active .bar:nth-child(1){
+
+transform:translateY(8px) rotate(45deg);
+
+}
+
+.hamburger.active .bar:nth-child(3){
+
+  transform:translateY(-8px) rotate(-45deg);
+
+}
